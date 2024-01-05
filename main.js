e9f336ac8f1628413590c0ee7c8f15b7e73adf56
vishal07316 <102129955+vishal07316@users.noreply.github.com>
2024-01-05 17:11:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
